0027c8d2d6574a50fd4c940d6db85494ab8f895c
Vaqef <vaqef.payfoon@outlook.com>
2020-12-09 22:10:52 +0330

    AddCustomer

--- Fastfood/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Fastfood/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace Fastfood.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     //بعد ار خرید سبد کالا
